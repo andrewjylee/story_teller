c679c5aa002905aa8b19e6ce41c4c7da39055ad6
Andrew Lee <andrewlee@am.local>
2016-03-25 00:58:54 -0400

    added data and init python file

---
+++ read_data.py
@@ -0,0 +1,14 @@
+
+import sys
+
+class data:
+    def __init__(self, filename):
+        self.filename = filename
+
+
+
+
+if __name__ == "__main__":
+    filename = 'test'
+    test = data(filename)
+    
