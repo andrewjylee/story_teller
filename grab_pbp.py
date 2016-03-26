f7512bb8142827ef50cb3abb422d7a9302a672b7
Andrew Lee <andrewlee@am.local>
2016-03-26 13:33:59 -0400

    cleaned up code into classes, keeps track of players' scores

--- grab_pbp.py
+++ grab_pbp.py
@@ -1,9 +1,48 @@
 import urllib2
 import string
 import re
+from data import html_parser
 
-" Fix last line? "
-"J. McRae makes 3-pt shot from 22 ft (assist by T. Thompson)+395-104 "
+class Team:
+    def __init__(self):
+        self.players = dict()
+        
+
+    def print_stats(self):
+        for k in self.players.keys():
+            print k, self.players[k]
+
+    def add_points(self, player, points):
+        if points == "2-pt":
+            points = 2
+        elif points == "3-pt":
+            points = 3
+        elif points == "free throw":
+            points = 1
+        else:
+            print "should never be here"
+
+        if player in self.players.keys():
+            self.players[player] += points
+        else:
+            self.players[player] = points
+
+    def score_tracker(self, line):
+        pattern = "^(\d+-\d+)\s\+?\d?[A-Z]\.\s(\w+)\smakes\s(\d-pt|free throw).*$"
+        match = re.search(pattern, line)
+
+        if match:
+            game_score = match.group(1)
+            player = match.group(2)
+            points = match.group(3)
+            self.add_points(player, points)
+
+    def stats_tracker(self, line):
+        if 'makes' in line:
+            self.score_tracker(line)
+
+
+
 
 def filter_tr(line):
     # Return tr_tag if line == <tr> or </tr>
@@ -47,23 +86,13 @@ def which_team(line):
 
        
 def clean_up(line):
-    print ''
     " Get score out first "
-    #match = re.search('">(.+?)</td>', line)
+    match = re.search('^.*>(\d+-\d+)</td>.*$', line)
 
-    #score = ''
-    #if match:
-    #    score = match.group(1)
-    #line = re.sub(score, '', line)
-
-    match = re.search("^.*(\d+)\-(\d+).*$", line)
-    #regex = re.compile("^.*[0-9]+\-[0-9]+.*$")
-    #if regex.match(line):
-    #    print 'here'
     score = ''
     if match:
-        print 'here'
         score = match.group(1)
+    line = re.sub(score, '', line)
 
     inbetween = 0
     ret = ""
@@ -79,33 +108,70 @@ def clean_up(line):
             continue
         ret += char
     
- 
-    score = ''
     ret = re.sub('&nbsp;', '', ret)
     return score, ret
 
-html = urllib2.urlopen("http://www.basketball-reference.com/boxscores/pbp/201603240BRK.html").read()
-start = 0
-lines = html.splitlines()
-for i in range(len(lines)):
-    line = lines[i]
-    if start:
-        if filter(line):
-            possession = line
-            side = which_team(possession)
-            if side == 1:
-                print clean_up(line)
-            elif side == 2:
-                print clean_up(line)
-            else:
-                print 'should never be here?'
-        continue
 
-    if "Start of 1st quarter" in line:
-        start = 1
-        continue
-    if "End of 4th quarter" in line:
-        print "END OF GAME"
-        break
+
+def get_winner(score):
+    pattern = "(\d+)-(\d+)"
+    match = re.search(pattern, score)
+    home_score = match.group(1)
+    away_score = match.group(2)
+    if home_score > away_score:
+        print "Home team won!"
+    else:
+        print "Away team won!"
+
+        
+
+def summarize(score, home, away):
+    get_winner(score)
+
+
+if __name__ == "__main__":
+
+    # TODO: make url out of user input of date and team
+    gameid = "201603240BRK"
+    html_class = html_parser(gameid)
+
+    start = 0
+    lines = html_class.html.splitlines()
+
+    home = Team()
+    away = Team()
+
+    for i in range(len(lines)):
+        line = lines[i]
+        if start:
+            if filter(line):
+                possession = line
+                side = which_team(possession)
+                if side == 1:
+                    score, summary = clean_up(line)
+                    home.stats_tracker(score + " " + summary)
+
+                elif side == 2:
+                    score, summary = clean_up(line)
+                    away.stats_tracker(score + " " + summary)
+                else:
+                    print 'should never be here?'
+            continue
+
+        # TODO: quarter-granularity?
+        if "Start of 1st quarter" in line:
+            start = 1
+            continue
+        # TODO: Overtime games
+        if "End of 4th quarter" in line:
+            print "END OF GAME"
+            break
+
+
+    summarize(score, home, away)
+    print '--'
+    home.print_stats()
+    print '--'
+    away.print_stats()
 
 
